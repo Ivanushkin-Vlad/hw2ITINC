d03ed2a5b0231ce022a91d822c202d5cb214df2d
Ivanushkin-Vlad <126400653+Ivanushkin-Vlad@users.noreply.github.com>
2023-11-11 15:51:07 +0200

    hw2 (HW5)

--- src/s2-homeworks/hw05/Pages.tsx
+++ src/s2-homeworks/hw05/Pages.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Routes, Route, Navigate } from 'react-router-dom'
+import {Routes, Route, Navigate, HashRouter} from 'react-router-dom'
 import Error404 from './pages/Error404'
 import PreJunior from './pages/PreJunior'
 import Junior from './pages/Junior'
@@ -14,20 +14,26 @@ export const PATH = {
 function Pages() {
     return (
         <div>
-            {/*Routes выбирает первый подходящий роут*/}
-            <Routes>
-                {/*роутинг будут писать студенты*/}
-                {/*в начале мы попадаем на страницу '/' и переходим сразу на страницу /pre-junior*/}
-                {/*<Route ...*/}
+                <Routes>
+                    {/*<Route path='/'/>*/}
+                    {/*роутинг будут писать студенты*/}
+                    {/*в начале мы попадаем на страницу '/' и переходим сразу на страницу /pre-junior*/}
+                    {/*<Route ...*/}
 
-                {/*роуты для /pre-junior, /junior, /junior-plus*/}
-                {/*<Route ...*/}
-                {/*<Route ...*/}
-                {/*<Route ...*/}
+                    {/*роуты для /pre-junior, /junior, /junior-plus*/}
+                    <Route path='/pre-junior'  element={<PreJunior/>}/>
+                    <Route path='/junior' element={<Junior/>}/>
+                    <Route path='/junior-plus' element={<JuniorPlus/>}/>
 
-                {/*роут для несуществующей страницы должен отрисовать <Error404 />*/}
-                {/*<Route ...*/}
-            </Routes>
+                    {/*<Route ...*/}
+                    {/*<Route ...*/}
+                    {/*<Route ...*/}
+
+                    {/*роут для несуществующей страницы должен отрисовать <Error404 />*/}
+
+                    <Route path='*' element={<Error404 />}/>
+                    {/*<Route ...*/}
+                </Routes>
         </div>
     )
 }
